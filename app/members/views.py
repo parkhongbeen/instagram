886ed9ee8ad02e5f1be2c270c41b8502dce19100
parkhongbeen <pack122@naver.com>
2019-12-24 18:37:57 +0900

    login_view구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('members:login')
     return render(request, 'members/login.html')
