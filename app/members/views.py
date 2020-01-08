f3bbc58c67902b622d4df587898cee460015029c
parkhongbeen <pack122@naver.com>
2020-01-08 15:02:57 +0900

    로그아웃구현

--- app/members/views.py
+++ app/members/views.py
@@ -1,9 +1,8 @@
-from http.client import HTTPResponse
-
-from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, get_user_model, logout
 from django.shortcuts import render, redirect
 
-# 장고 기본유저나
+# 장고 기본유저나 Custom유저모델 중, 사용중인 User모델을 가져옴
 User = get_user_model()
 
 
@@ -26,13 +25,10 @@ def signup_view(request):
         action만 이쪽으로
     URL: /members/signup/
     User에 name필드를 추가
-        email
-        username
-        name
-        password
+        email, username, name, password
     를 전달받아, 새로운 User를 생성한다.
     생성시, User.objects.create_user() 메서드를 사용한다.
-    이미 존재하는 username또는 email을 입력한 경우 "이미 사용중인 username/email입니다"라는 메시지를  HttpResponsefh ehffuwnsek.
+    이미 존재하는 username또는 email을 입력한 경우 "이미 사용중인 username/email입니다"라는 메시지를  HttpResponse로 돌려준다.
     생성에 성공하면 로그인 처리 후 (위의 login_view를 참조) posts:post-list로 redirect처리
     """
     # 사용자한테 입력받은 값
@@ -42,9 +38,9 @@ def signup_view(request):
     password = request.POST['password']
 
     if User.objects.filter(username=username).exists():
-        return HTTPResponse('이미 사용중인 username입니다.')
+        return HttpResponse('이미 사용중인 username입니다.')
     if User.objects.filter(email=email).exists():
-        return HTTPResponse('이미 사용중인 email입니다.')
+        return HttpResponse('이미 사용중인 email입니다.')
 
     user = User.objects.create_user(
         user=username,
@@ -54,3 +50,16 @@ def signup_view(request):
     )
     login(request, user)
     return redirect('posts:post-list')
+
+
+def logout_view(request):
+    """
+    GET요청으로 처리함
+    요청에 있는 사용자를 logout처리
+    djaong.contrib.auth.logout함수를 사용한다.
+
+    URL: /members/logout/
+    Template: 없음
+   """
+    logout(request)
+    return redirect('members:login')
