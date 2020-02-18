a25a106afc11c3a2d3fcf5187869978e7e08f96d
parkhongbeen <pack122@naver.com>
2020-02-18 16:00:35 +0900

    PostListCreateAPIView와 GET(list)요청 구현

--- app/posts/apis.py
+++ app/posts/apis.py
@@ -1,14 +1,11 @@
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-from members.serializers import UserSerializer
 from posts.models import Post
 from posts.serializer import PostSerializer
 
 
 class PostListCreateAPIView(APIView):
     def get(self, request):
-        return Response('aaa')
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
